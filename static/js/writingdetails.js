f7376b558500510d1833cb6c7875a3355c3898da
younghyun-na <younghyunna@naver.com>
2021-11-18 03:56:14 +0900

    refactor: 글 상세 프로필 사진 추가

--- static/js/writingdetails.js
+++ static/js/writingdetails.js
@@ -1,3 +1,4 @@
+/*
 // 댓글 기능
 const inputBar = document.querySelector("#comment-input"); 
 const rootDiv = document.querySelector("#comments"); 
@@ -14,20 +15,6 @@ function generateTime(){
     const min = date.getMinutes(); 
     const sec = date.getSeconds(); 
     const time = year+'-'+month+'-'+wDate+' '+hour+':'+min+':'+sec; return time; 
-} 
-
-//유저이름 발생기 
-function generateUserName(){ 
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'; 
-    var makeUsername = ''; 
-    for(let i=0; i<4;i++){ 
-        let index = Math.floor(Math.random(10) * alphabet.length); 
-        makeUsername += alphabet[index]; 
-    } 
-    for(let j=0;j<4;j++){ 
-        makeUsername += "*"; 
-    } 
-    return makeUsername; 
 } 
 
 
@@ -55,9 +42,7 @@ function showComment(comment){
     const modifyBtn = document.createElement('button'); 
     const spacer = document.createElement('div');
 
-    const newId = idOrVoteCountList.length+1;
 
-    // 스코프 밖으로 나가는 순간 하나지우면 다 지워지고 입력하면 리스트 다불러옴.
     // 삭제버튼 만들기 
     const delBtn = document.createElement('button'); 
     delBtn.className ="deleteComment"; 
@@ -72,24 +57,15 @@ function showComment(comment){
     userName.appendChild(delBtn); 
     //입력값 넘기기 
     inputValue.innerText = comment; 
-    //타임스템프찍기 
+    //타임스템프
     showTime.innerHTML = generateTime(); 
     countSpan.innerHTML=0; 
-    //투표창 만들기, css먼저 입혀야함. 
-    voteUp.id = "voteUp"; 
-    voteUp.innerHTML = '↑'; 
-    voteDown.id = "voteDown"; 
-    voteDown.innerHTML = '↓'; 
-    voteDiv.appendChild(voteUp); 
-    voteDiv.appendChild(voteDown); 
-    //댓글뿌려주기 
+
+    //댓글 
     commentList.appendChild(userName); 
     commentList.appendChild(inputValue); 
     commentList.appendChild(showTime); 
-    commentList.appendChild(voteDiv); 
     rootDiv.prepend(commentList); 
-    voteUp.addEventListener("click",numberCount); 
-    voteDown.addEventListener("click",numberCount); 
     delBtn.addEventListener("click",deleteComments); 
     console.dir(rootDiv); 
 } 
@@ -107,46 +83,6 @@ function pressBtn(){
 } 
 
 btn.onclick = pressBtn;
-
-
-
-/*
-function warnEmpty(){
-    alert("댓글을 입력해주세요");
-}
-
-function dateToString(date){
-    const dateString = date.toISOString();
-    const dateToString = dateString.substring(11,19);
-    return dateToString;
-}
-
-function submitComment(){
-    const newcommentEL = document.getElementById("comment-input");
-    const newcomment = newcommentEL.value.trim();
-
-    if(newcomment){
-        const dateEL = document.createElement('div');
-        dateEL.classList.add("comment-date");
-        const dateString = dateToString(new Date());
-        dateEL.innerText = dateString;
-
-        const contentEL = document.createElement('div');
-        contentEL.classList.add("comment-content");
-        contentEL.innerText = newcomment;
-
-        const commentEL = document.createElement('div');
-        commentEL.classList.add("comment-row");
-        commentEL.appendChild(dateEl);
-        commentEL.appendChild(contentEL);
-
-        document.getElementById("comments").appendChild(commentEL);
-        newcommentEL.value="";
-
-        const countEL = document.getElementById("comments-count");
-        const count = countEL.innerText;
-        countEL.innerText = parseInt(count) + 1;
-    }
-    else warnEmpty();
-}
 */
+
+
